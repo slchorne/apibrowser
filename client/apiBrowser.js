63e566b256d938c4f1be99da28a3a8aaaaef11a5
Geoff H <horne@slc.com.au>
2016-03-04 15:43:35 -0800

    basic auth now working

--- client/apiBrowser.js
+++ client/apiBrowser.js
@@ -37,7 +37,7 @@ var wapi = {
 };
 
 // localstorage handlers, built as a factory
-viewApp.factory("LS", function($window, $rootScope) {
+viewApp.factory("$localStorage", function($window, $rootScope) {
   return {
     setData: function(val) {
       var jval = angular.toJson(val);
@@ -51,14 +51,15 @@ viewApp.factory("LS", function($window, $rootScope) {
   };
 });
 
-viewApp.controller('mainController', function($scope,$http,LS,$filter) {
+viewApp.controller('mainController',
+    function($scope,$http,$localStorage,$filter,Base64) {
 
     // create scope variables that link to the view(s)
     $scope.formFields = {};
     $scope.searchFields = {};
 
     //
-    var vls = LS.getData();
+    var vls = $localStorage.getData();
     $scope.formFields = vls ;
     console.log ( 'load local storage', vls );
 
@@ -70,9 +71,17 @@ viewApp.controller('mainController', function($scope,$http,LS,$filter) {
 
     // the initial login function
     $scope.saveUser = function() {
-        console.log( 'save user', $scope.formFields );
+        var cred = $scope.formFields;
+        console.log( 'save user', cred );
 
-        LS.setData($scope.formFields);
+        // create the basic auth
+        var credentials = btoa( cred.name + ':' + cred.password );
+        var authorization = {'Authorization': 'Basic ' + credentials};
+        wapi.headers = { headers: authorization };
+
+        console.log ( 'header', wapi.headers);
+
+        $localStorage.setData($scope.formFields);
 
         // generate the URL for the server
         // to avoid XSS problems we punt to ourselves, with a proxy URL
@@ -88,13 +97,15 @@ viewApp.controller('mainController', function($scope,$http,LS,$filter) {
         // as the schema returns version specific data
 
         // then make a request to get the schema
-        $http.get(wapi.url +'?_schema' )
+        // we should only have to set the headers once,
+        // the cookie should do the rest
+        $http.get(wapi.url +'?_schema' , wapi.headers )
             .then(function(response){
                 // success
                 console.log( 'schema' , response.data );
                 console.log( 'wapi' , wapi );
-                $scope.$parent.checkSchema(response.data);
-                $scope.$parent.listObjects(response.data);
+                $scope.checkSchema(response.data);
+                $scope.listObjects(response.data);
 
                 // $scope.$parent.goTo('objects',response.data);
                 // console.log('switched');
@@ -103,11 +114,6 @@ viewApp.controller('mainController', function($scope,$http,LS,$filter) {
                 // error, HTTP errors
                 console.log( 'HTTP error' );
             });
-
-        // because we're inside the template, we need to switch
-        // $scope to the $parent
-        //$scope.goTo('objects');
-        // $scope.$parent.goTo('objects');
 
     };
 
@@ -244,4 +250,90 @@ viewApp.controller('mainController', function($scope,$http,LS,$filter) {
     };
     /*
     */
+});
+
+viewApp.factory('Base64', function () {
+    /* jshint ignore:start */
+
+    var keyStr = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=';
+
+    return {
+        encode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            do {
+                chr1 = input.charCodeAt(i++);
+                chr2 = input.charCodeAt(i++);
+                chr3 = input.charCodeAt(i++);
+
+                enc1 = chr1 >> 2;
+                enc2 = ((chr1 & 3) << 4) | (chr2 >> 4);
+                enc3 = ((chr2 & 15) << 2) | (chr3 >> 6);
+                enc4 = chr3 & 63;
+
+                if (isNaN(chr2)) {
+                    enc3 = enc4 = 64;
+                } else if (isNaN(chr3)) {
+                    enc4 = 64;
+                }
+
+                output = output +
+                    keyStr.charAt(enc1) +
+                    keyStr.charAt(enc2) +
+                    keyStr.charAt(enc3) +
+                    keyStr.charAt(enc4);
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+            } while (i < input.length);
+
+            return output;
+        },
+
+        decode: function (input) {
+            var output = "";
+            var chr1, chr2, chr3 = "";
+            var enc1, enc2, enc3, enc4 = "";
+            var i = 0;
+
+            // remove all characters that are not A-Z, a-z, 0-9, +, /, or =
+            var base64test = /[^A-Za-z0-9\+\/\=]/g;
+            if (base64test.exec(input)) {
+                window.alert("There were invalid base64 characters in the input text.\n" +
+                    "Valid base64 characters are A-Z, a-z, 0-9, '+', '/',and '='\n" +
+                    "Expect errors in decoding.");
+            }
+            input = input.replace(/[^A-Za-z0-9\+\/\=]/g, "");
+
+            do {
+                enc1 = keyStr.indexOf(input.charAt(i++));
+                enc2 = keyStr.indexOf(input.charAt(i++));
+                enc3 = keyStr.indexOf(input.charAt(i++));
+                enc4 = keyStr.indexOf(input.charAt(i++));
+
+                chr1 = (enc1 << 2) | (enc2 >> 4);
+                chr2 = ((enc2 & 15) << 4) | (enc3 >> 2);
+                chr3 = ((enc3 & 3) << 6) | enc4;
+
+                output = output + String.fromCharCode(chr1);
+
+                if (enc3 != 64) {
+                    output = output + String.fromCharCode(chr2);
+                }
+                if (enc4 != 64) {
+                    output = output + String.fromCharCode(chr3);
+                }
+
+                chr1 = chr2 = chr3 = "";
+                enc1 = enc2 = enc3 = enc4 = "";
+
+            } while (i < input.length);
+
+            return output;
+        }
+    };
+
+    /* jshint ignore:end */
 });
