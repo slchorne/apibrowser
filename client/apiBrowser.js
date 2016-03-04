5914aa275a1a950be388be2eff77dedaf803df95
Geoff H <horne@slc.com.au>
2016-03-04 13:44:45 -0800

    basic working search version

--- client/apiBrowser.js
+++ client/apiBrowser.js
@@ -3,7 +3,7 @@
 // create the module and name it viewApp
 // also include the router module for some later fancy stuff we aren't
 // using now
-var viewApp = angular.module('viewApp', []);
+var viewApp = angular.module('viewApp', ['jsonFormatter']);
 
 // [ ] we can't use views, as the context scope keeps switching
 // we should try to use just show and hide
@@ -36,7 +36,7 @@ var wapi = {
     maxVersion : '2.0'
 };
 
-// local storage handlers
+// localstorage handlers, built as a factory
 viewApp.factory("LS", function($window, $rootScope) {
   return {
     setData: function(val) {
@@ -51,10 +51,11 @@ viewApp.factory("LS", function($window, $rootScope) {
   };
 });
 
-viewApp.controller('mainController', function($scope,$http,LS) {
+viewApp.controller('mainController', function($scope,$http,LS,$filter) {
 
     // create scope variables that link to the view(s)
     $scope.formFields = {};
+    $scope.searchFields = {};
 
     //
     var vls = LS.getData();
@@ -96,7 +97,7 @@ viewApp.controller('mainController', function($scope,$http,LS) {
                 $scope.$parent.listObjects(response.data);
 
                 // $scope.$parent.goTo('objects',response.data);
-                console.log('switched');
+                // console.log('switched');
 
             },function(response){
                 // error, HTTP errors
@@ -120,19 +121,86 @@ viewApp.controller('mainController', function($scope,$http,LS) {
         console.log ( 'max rev', wapi);
     };
 
-    // handler to render the list of objects
+    //
+    // handler to render the list of objects to work on
+    //
     $scope.listObjects = function(data) {
         // hardcode the view
-        // console.log( 'switch to objects view ' , data );
+        // then just expose the data to the HTML
+        // and process it there
         $scope.objects = data.supported_objects;
         $scope.template = "objects.html";
         $scope.message = 'This is the settings page';
     };
 
-    $scope.searchObject = function(myObj) {
+    //
+    // handler for when we input search values to generate
+    // a real-time query string.
+    // -- GNDN for now --
+    //
+    $scope.generateSearchQuery = function(el) {
+        console.log('field change',el);
+    };
+
+    //
+    // do the specific object search
+    //
+    $scope.searchForObject = function(el) {
+        // console.log('search fields',$scope.schemaFields);
+
+        // the html will add a new field to the schema 'fieldValue'
+        // and some modifier flags
+        // which we can use to extract the values and form a query string
+
+        // We have to process each name value pair and build up a query string
+        // so we may as well just walk the whole array
+        // and not start with a filter
+
+        // var useFields = $scope.schemaFields.filter(function(a) {
+        //     return a.fieldValue ? true : false; });
+
+        var qs = [];
+        angular.forEach($scope.schemaFields, function(field){
+            if ( field.fieldValue ) {
+                var qv = field.name ;
+                qv += field.modCase ? ':' : null ;
+                qv += field.modContains ? '~' : null ;
+                qv += '=' + field.fieldValue;
+                qs.push(qv);
+                console.log ('qv', qv);
+            }
+        });
+        var queryString = qs.join('&');
+        // reset the search url
+        $scope.searchUrl = wapi.url + $scope.myObject + '?' + queryString ;
+
+        // and punt to a search
+        $http.get( $scope.searchUrl )
+            .then(function(response){
+                // success
+                console.log( 'object search ' , response.data );
+
+                // then just expose the data to the HTML
+                // and process it there
+                $scope.searchResults = response.data;
+
+            },function(response){
+                // error, HTTP errors
+                console.log( 'HTTP error' );
+            });
+
+
+    };
+
+    //
+    // handler to make the object search form and page
+    //
+    $scope.searchObjectPage = function(myObj) {
         console.log( 'switch to search ' , myObj );
         $scope.myObject = myObj;
         $scope.template = "search.html";
+        $scope.searchUrl = wapi.url + myObj ;
+        $scope.searchResults = null ;
 
         // define some filters for listing the schema
             // return element.name.match(/=/) ? true : false;
@@ -141,22 +209,32 @@ viewApp.controller('mainController', function($scope,$http,LS) {
             // console.log ( 'filter elem', element );
             return element.searchable_by ? true : false;
         };
+        $scope.modifierCase = function(element) {
+            return element.searchable_by.match(/:/) ? true : false;
+        };
+        $scope.modifierContains = function(element) {
+            return element.searchable_by.match(/~/) ? true : false;
+        };
 
-        // get the schema for this object
+        // get the schema for this object and expose it to the html
         $http.get(wapi.url + myObj + '?_schema' )
             .then(function(response){
                 // success
                 console.log( 'object schema' , response.data );
-                $scope.schema = response.data.fields;
+
+                // then just expose the data to the HTML
+                // and process it there
+                $scope.schemaFields = response.data.fields;
 
             },function(response){
                 // error, HTTP errors
                 console.log( 'HTTP error' );
             });
+
     };
 
     // and now a function to switch into different templates without
-    // using routes, used by the navbar
+    // using routes, this is used by the navbar
     //
     $scope.goTo = function(myView,data) {
         console.log( 'switch to view ' , myView , ' scope:' , $scope );
