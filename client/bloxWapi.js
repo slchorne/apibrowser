ff7431142f387c2d404061c0f4be4cfac5a50c62
Geoff H <horne@slc.com.au>
2016-03-11 15:41:53 -0800

    refactor wapi calls into a module

--- client/bloxWapi.js
+++ client/bloxWapi.js
@@ -2,73 +2,187 @@
 
 //
 // do most stuff as a 'service' service, to keep it out of the controller
+// and the global namespace
 // http://tylermcginnis.com/angularjs-factory-vs-service-vs-provider/
 // (services don't have namespace hardcoding), and instintiate copies
 //
 // ideally we want an app agnostic module
 // angular.module('jsonFormatter', ['RecursionHelper'])
 
-// WAPI handlers, as a service ?
+//
+// WAPI handlers, as a service :
 // http://weblogs.asp.net/dwahlin/using-an-angularjs-factory-to-interact-with-a-restful-service
-// However, as the .then() call has to live outside the service
-// there may not be much value to the service,
-// but you could bundle the error handler getErrorMsg()
-// wapiService.get(url,successCallback);
-
-// [ ] and you can move the wapi globals and handlers out of
-// the global namespace
-
-// Global state variables
-var wapi = {
-    server: null,
-    url: null,
-    proxy: '/wapip/',
-    version: '2.0',
-    maxVersion : null,
-};
-
+// this keeps the URL and headers internal to this module
+// and you can bundle the error handler getErrorMsg()
 //
-// initial global functions, outside of $scope to
-// avoid race conditions with the parser
-//
-var setAuthHeader = function(authkey) {
-    var authorization = {'Authorization': 'Basic ' + authkey};
-    wapi.headers = {
-        withCredentials: true,
-        headers: authorization };
-    return wapi.headers;
-};
+// wapi.get(path).then()
 
-var getErrorMsg = function(response) {
-    // Errors can be text, HTML, XML or json,
-    // so we try and parse them as best we can
-    console.log ( 'getErrorMsg:resp',response);
+angular.module('wapiModule', [])
+    // private values
+    .value('wapiConfig',{
+        server: null,
+        url: null,
+        proxy: '/wapip/',
+        version: '2.0',
+        maxVersion : null,
+    })
 
-    var msg = response.data ;
 
-    // and angular tries to autotransform the data, so it may or may
-    // not be an object already from something that may have been json
-    if ( response.status == 401 ) {
+    // remember to pass in the values to the service
+    .service('wapi',function($http,wapiConfig){
+        var me = this ;
 
-    }
+        //----------------------
+        // private functions (well, this is a service, there is no privacy)
+        // called as 'me.func()'
 
-    if ( response.data.syscall
-     || response.data.data
-     || response.data.Error ) {
-         // we are probably a clean object
-         msg = angular.toJson( response.data );
-         if ( response.data.Error ) {
-             msg = response.data.Error;
-         }
-    }
+        this.test = function(msg) {
+            console.log ('wapi.test :', msg);
+        };
 
-    if ( response.status == 401 ) {
-        msg = "Authorization Required: You supplied the wrong credentials (e.g., bad password)";
+        //----------------------
+        // public functions
 
-    }
+        this.getConfig = function() {
+            return wapiConfig ;
+        };
 
-    // anything else can be taken verbatim no need to convert
-    //  if ( msg.match(/DOCTYPE HTML/)) {
+        //
+        // -- [ ] GNDN ?
+        this.getUrl = function() {
+            // var conf = me.getConfig();
+            return me.getConfig().url ;
+        };
 
-    return response.status + " : " + msg ;
-};
+        this.getPath = function() {
+            var conf = me.getConfig();
+            return conf.server +
+                '/wapi/v' + conf.version + '/';
+        };
+
+        this.getAuthKey = function() {
+            console.log ( 'ak' , me.getConfig() );
+            return me.getConfig().authkey ;
+        };
+
+        //
+        // change the proxy, which changes the URL
+        this.setProxy = function(proxy) {
+            var conf = me.getConfig();
+            conf.proxy = proxy;
+            // call setVersion which sets the URL
+            me.setVersion();
+        };
+
+        // set the WAPI version and update the url
+        // if called with no args, use the default
+        this.setVersion = function(rev) {
+            var conf = me.getConfig();
+            if ( rev ) {
+                conf.maxVersion = rev;
+                conf.version = rev;
+            }
+
+            // to avoid XSS problems we punt to ourselves, with a proxy URL
+            // and we can switch that at any time by changing the proxy.
+
+            conf.url = conf.proxy + conf.server +
+                '/wapi/v' + conf.version + '/';
+        };
+
+        //
+        // configure and save the all the REST settings
+        // we are passed in an object :
+        // {
+        //    server : REQUIRED
+        //    name : REQUIRED
+        //    password : Optional
+        //    authkey :  Optional
+        //  }
+        //
+
+        this.session = function(args) {
+            // generate the Authorization and keys
+            var credentials = args.authkey ;
+            if ( args.name && args.password ) {
+                credentials = btoa( args.name + ':' + args.password );
+            }
+            var authorization = {'Authorization': 'Basic ' + credentials };
+
+            // save it all
+            var conf = me.getConfig();
+
+            conf.server = args.server ;
+            conf.user = args.name ;
+            conf.authkey = credentials ;
+
+            me.setVersion();
+
+            // we may need to make this another method, depending on where
+            // we have to call it
+            conf.httpConfig = {
+                withCredentials: true,
+                headers: authorization
+            };
+
+            // console.log ('session', conf , args );
+        };
+
+        //
+        // remove any authkeys, reset any cookies
+        //
+        this.logout = function() {
+            var conf = me.getConfig();
+            me.session({
+                server : conf.server
+            });
+        };
+
+        // [ ] wrapped $http handlers
+        // just append the path (and ? queryString ) to the config items
+        // return the $promise so we can use '.then()'
+        this.get = function( path ) {
+            var conf = me.getConfig();
+            return $http.get(conf.url + path , conf.httpConfig );
+        };
+
+        //
+        // error re-formatting handlers
+        //
+        this.getErrorMsg = function(response) {
+            // Errors can be text, HTML, XML or json,
+            // so we try and parse them as best we can
+            console.log ( 'getErrorMsg:resp',response);
+
+            var msg = response.data ;
+
+            // and angular tries to autotransform the data, so it may or may
+            // not be an object already from something that may have been json
+            if ( response.status == 401 ) {
+
+            }
+
+            if ( response.data.syscall ||
+                 response.data.data ||
+                 response.data.Error ) {
+                 // we are probably a clean object
+                 msg = angular.toJson( response.data );
+
+                 if ( response.data.Error ) {
+                     msg = response.data.Error;
+                 }
+            }
+
+            if ( response.status == 401 ) {
+                msg = "Authorization Required: You supplied the wrong credentials (e.g., bad password)";
+
+            }
+
+            // anything else can be taken verbatim no need to convert
+            //  if ( msg.match(/DOCTYPE HTML/)) {
+
+            return response.status + " : " + msg ;
+        };
+
+        return this ;
+});
